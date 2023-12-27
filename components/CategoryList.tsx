82dd301022d4e6a1f09b5e00b8355224a3ad4aba
iitzrohan <t.rohan1903@gmail.com>
2023-12-28 05:08:34 +0530

    Add featured post and component category design

--- components/CategoryList.tsx
+++ components/CategoryList.tsx
@@ -1,7 +1,81 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 const CategoryList = () => {
-  return <div>CategoryList</div>;
+  return (
+    // Container
+    <div>
+      {/* Title */}
+      <h1 className="my-12">Popular Categories</h1>
+      {/* Categories */}
+      <div className="flex flex-wrap justify-between gap-5">
+        {/* Category */}
+        <Link
+          href="/blog?cat=style"
+          className="flex items-center gap-2 capitalize w-full sm:w-2/5 md:w-1/4 lg:w-1/5 xl:w-36 h-20 rounded justify-center bg-[#57c4ff31]"
+        >
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          style
+        </Link>
+        <Link
+          href="/blog?cat=style"
+          className="flex items-center gap-2 capitalize w-full sm:w-2/5 md:w-1/4 lg:w-1/5 xl:w-36 h-20 rounded justify-center bg-[#da85c731]"
+        >
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          fashion
+        </Link>
+        <Link
+          href="/blog?cat=style"
+          className="flex items-center gap-2 capitalize w-full sm:w-2/5 md:w-1/4 lg:w-1/5 xl:w-36 h-20 rounded justify-center bg-[#7fb88133]"
+        >
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          food
+        </Link>
+        <Link
+          href="/blog?cat=style"
+          className="flex items-center gap-2 capitalize w-full sm:w-2/5 md:w-1/4 lg:w-1/5 xl:w-36 h-20 rounded justify-center bg-[#ff795736]"
+        >
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          travel
+        </Link>
+        <Link
+          href="/blog?cat=style"
+          className="flex items-center gap-2 capitalize w-full sm:w-2/5 md:w-1/4 lg:w-1/5 xl:w-36 h-20 rounded justify-center bg-[#ffb04f45]"
+        >
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          culture
+        </Link>
+        <Link
+          href="/blog?cat=style"
+          className="flex items-center gap-2 capitalize w-full sm:w-2/5 md:w-1/4 lg:w-1/5 xl:w-36 h-20 rounded justify-center bg-[#5e4fff31]"
+        >
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          coding
+        </Link>
+      </div>
+      CategoryList
+    </div>
+  );
 };
 
 export default CategoryList;
--- components/Featured.tsx
+++ components/Featured.tsx
@@ -1,7 +1,40 @@
+import Image from "next/image";
 import React from "react";
+import { Button } from "./ui/button";
 
 const Featured = () => {
-  return <div>Featured</div>;
+  return (
+    // Container
+    <div className="mt-7">
+      {/* Title */}
+      <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-light">
+        <b>Hey, Rohan here!</b> Discover my stories and creative ideas.
+      </h1>
+      {/* Post Container */}
+      <div className="mt-14 flex items-center gap-12">
+        {/* Img Container */}
+        <div className="hidden lg:block flex-1 h-[500px] relative">
+          <Image src="/p1.jpeg" alt="" fill className="object-cover" />
+        </div>
+        {/* Text Container */}
+        <div className="flex flex-1 flex-col gap-5">
+          {/* Post Title */}
+          <h1 className="text-4xl">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius,
+            sunt.
+          </h1>
+          {/* Post Description */}
+          <p className="text-xl font-light">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde minus
+            in tempore aspernatur, commodi laudantium. Vero officiis
+            consequuntur libero doloribus! Nisi beatae aspernatur minus. Unde
+            repellendus eveniet quia officiis ab.
+          </p>
+          <Button className="w-max">Read more</Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
