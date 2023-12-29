f288384dc191bcdf6ad77b01bba4c5c471101fb2
iitzrohan <t.rohan1903@gmail.com>
2023-12-30 00:35:07 +0530

    Add pagination

--- components/CardList.tsx
+++ components/CardList.tsx
@@ -1,6 +1,14 @@
 import React from "react";
-import Pagination from "./Pagination";
 import Card from "./Card";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 
 const CardList = () => {
   return (
@@ -18,7 +26,18 @@ const CardList = () => {
           <Card />
         </div>
       </div>
-      <Pagination />
+      <Pagination>
+        <PaginationContent>
+          <PaginationPrevious href="/" />
+          <PaginationLink href="/" isActive>
+            1
+          </PaginationLink>
+          <PaginationLink href="/">2</PaginationLink>
+          <PaginationLink href="/">3</PaginationLink>
+          <PaginationEllipsis />
+          <PaginationNext href="/" />
+        </PaginationContent>
+      </Pagination>
     </div>
   );
 };
